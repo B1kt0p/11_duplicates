15e24c09309e4815c9d71be400cdcbfed592538c
buktop <buktop>
2018-01-25 22:06:47 +0900

    Fixed variable names. Removed useless exceptions.

--- duplicates.py
+++ duplicates.py
@@ -10,42 +10,38 @@ def get_parse_argv():
     parse.add_argument(
         '--directory',
         '-d',
-        help='path to root directory'
+        help='path to root directory',
+        type=str
     )
     return parse.parse_args()
 
 
 def get_full_files_path(root_dir_path):
     all_file_paths = defaultdict(list)
-    for root, dirs, files in os.walk(root_dir_path):
-        for file in files:
-            try:
-                full_file_path = os.path.join(root, file)
-                size_file = os.path.getsize(full_file_path)
-            except (UnicodeEncodeError, FileNotFoundError):
-                print ('Не могу прочитать {}'.format(file))
-                continue
-            all_file_paths[(file, size_file)].append(full_file_path)
+    for root, dir_name, file_names in os.walk(root_dir_path):
+        for file_name in file_names:
+            full_file_path = os.path.join(root, file_name)
+            size_file = os.path.getsize(full_file_path)
+            all_file_paths[(file_name, size_file)].append(full_file_path)
     return all_file_paths
 
 
 def get_dublicate_files(all_file_paths):
     dublicate_files = {}
-    for file in all_file_paths:
-        if len(all_file_paths[file]) > 1:
-            dublicate_files.update({file: all_file_paths[file]})
+    for file_names in all_file_paths:
+        if len(all_file_paths[file_names]) > 1:
+            dublicate_files.update({file_names: all_file_paths[file_names]})
     return dublicate_files
 
 
 def print_dublicate_files(dublicate_files):
     print('Найдены файлы с одинаковым именем и размером:')
-    for file_name, file_size in dublicate_files:
-        print('{} ({} байт):'.format(file_name, file_size))
-        [print("\t\t{}".format(file_path)) for file_path in dublicate_files[(file_name, file_size)]]
+    for file_name, file_paths in dublicate_files.items():
+        print('{} ({} байт):'.format(*file_name))
+        [print("\t\t{}".format(file_path)) for file_path in file_paths]
 
 
 if __name__ == '__main__':
-    pass
     root_dir_path = get_parse_argv().directory
     if os.path.isdir(root_dir_path):
         all_file_paths = get_full_files_path(root_dir_path)
@@ -55,4 +51,4 @@ if __name__ == '__main__':
         else:
             print('Повторяющихся файлов не обнаружено')
     else:
-        print('Неверное имя директории. ВВедите верное имя')
+        print('Неверное имя директории. Введите верное имя')
